f73cc883d8f1076c0e542ff59b5aa65bf3c9fbf7
Jack D. You <gmyou71@gmail.com>
2017-02-26 12:28:29 +0900

    add if && froelse

--- dev-develop/resources/views/index.blade.php
+++ dev-develop/resources/views/index.blade.php
@@ -17,4 +17,17 @@ h1, h2 { border-bottom:1px solide #eee; }
   @endforeach
 </ul>
 
+@if($itemCount = count($items))
+  <p>There are {{ $itemCount }} items !</p>
+@else
+  <p>There is no item !</p>
+@endif
+
+@forelse($items as $item)
+  <p>The item is {{ $item }}</p>
+@empty
+  <p>There is no item !</p>
+@endforelse
+
+
 </body>
