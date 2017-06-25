ea3bb67c7fcc92a81b1035d0cd26849741ec5b3c
gmyou <gmyou71@gmail.com>
2017-06-26 05:03:54 +0900

    remove comment

--- dev-develop/app/Http/Controllers/ArticlesController.php
+++ dev-develop/app/Http/Controllers/ArticlesController.php
@@ -6,77 +6,36 @@ use Illuminate\Http\Request;
 
 class ArticlesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         return __METHOD__ . ' Article 컬렉션 조회';
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return __METHOD__ . ' Article 컬렉션 생성폼';
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         return __METHOD__ . ' Article 컬렉션 생성';
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         return __METHOD__ . ' Article 컬렉션 조회'.$id;
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         return __METHOD__ . ' Article 컬렉션 수정폼';
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         return __METHOD__ . ' Article 컬렉션 수정';
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         return __METHOD__ . ' Article 컬렉션 삭제'.$id;
